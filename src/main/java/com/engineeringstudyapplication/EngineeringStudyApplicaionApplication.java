e5486ebfb5fcb5f62713233a75c736328160743f
rutikbodke333 <rutikbodke333@gmail.com>
2025-06-10 00:07:19 +0530

    folder updated

---
+++ src/main/java/com/engineeringstudyapplication/EngineeringStudyApplicaionApplication.java
@@ -0,0 +1,13 @@
+package com.engineeringstudyapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngineeringStudyApplicaionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngineeringStudyApplicaionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/engineeringstudyapplication/EngineeringStudyApplicaionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engineeringstudyapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngineeringStudyApplicaionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
